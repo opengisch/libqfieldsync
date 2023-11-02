ef1b6c1b771544b9c17ca982233ec0dcf5e5e474
Ivan Ivanov <ivan.ivanov@suricactus.com>
2023-11-02 16:56:19 +0200

    Remove imports in the `__init__.py` file
    Importing from `__init__.py` does not work with QFieldSync plugin reloader.

--- libqfieldsync/__init__.py
+++
@@ -1,4 +0,0 @@
-from .layer import LayerSource, SyncAction
-from .offline_converter import OfflineConverter
-from .project import ProjectConfiguration, ProjectProperties
-from .utils import exceptions, file_utils
