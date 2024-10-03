5f6c200faea6e35acbdf8a07a7c6880349bdb42f
Johnny Sequeira <seqlazjhonny@gmail.com>
2024-10-03 16:30:26 -0600

    Removing commented code

--- libqfieldsync/utils/qgis.py
+++ libqfieldsync/utils/qgis.py
@@ -52,8 +52,7 @@ def make_temp_qgis_file(project: QgsProject) -> str:
     project_backup_dir = tempfile.mkdtemp()
     original_filename = project.fileName()
     backup_filename = os.path.join(project_backup_dir, f"{project.baseName()}.qgs")
-    # NOTE: This makes the conversion of the project mess with the datasource on Windows
-    # project.write(backup_filename)
+    project.write(backup_filename)
     project.setFileName(original_filename)
 
     return backup_filename
